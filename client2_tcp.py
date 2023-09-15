16fc41c629cbc565c3b1cc981ac554b4776a649e
pranav-rajj <pranavraju10@gmail.com>
2023-09-15 11:29:36 +0530

    initial commit

---
+++ client2_tcp.py
@@ -0,0 +1,103 @@
+from socket import *
+import threading
+import json
+import os
+
+server_name = 'localhost'
+server_port = 12004
+mutex = threading.Lock()
+
+client = socket(AF_INET , SOCK_STREAM)
+client.connect((server_name,server_port))
+
+Name =""
+def new_name():
+    global Name
+    Name = input("Enter your name: ")
+
+#handles connection with server
+def handler():
+    new_name()
+    while True:
+        received_message = client.recv(1024).decode('utf-8')
+        received_message = received_message.strip()
+        if received_message == 'Name':
+            client.send(Name.encode('utf-8'))
+        elif received_message == 'Retry':
+            print("Name already exists,try again")
+            new_name()
+        elif received_message == 'Terminate':
+            exit()
+        elif received_message == 'Success':
+            break
+        else :
+            print("Something went wrong")
+            exit()
+
+stop_thread = False
+
+#function for receiving messages
+def receive():
+    global stop_thread
+    while True:
+        received_message = client.recv(1024).decode('utf-8')
+        if not received_message:
+            stop_thread = True        
+        if stop_thread:
+            break
+        decoded_message = json.loads(received_message)
+        if decoded_message[0] == 1:
+            if decoded_message[1] == 0:
+                print(f"{decoded_message[2]} has left the chat!")
+            elif decoded_message[1] == 1:
+                print(f"{decoded_message[2]} joined the chat")
+            else:
+                print(f"{decoded_message[2]}: {decoded_message[3]}")
+        elif decoded_message[0] == 2:
+            mutex.acquire()
+            with open(f"/home/pranav/coding/cn/Socket_programming/{decoded_message[2]}",'wb') as image_file:
+                file_len = int(client.recv(1024).decode('utf-8').strip())
+                while file_len > 0:
+                    data = client.recv(1024)
+                    file_len -= len(data)
+                    if not data:
+                        break
+                    image_file.write(data)
+                print(f'The file has been received form {decoded_message[1]}')
+            mutex.release()
+
+#function for sending messages
+def send():
+    global stop_thread
+    while True:
+        if stop_thread:
+            break
+        task = int(input(""))
+        if task == 1:
+            client.send("1".encode('utf-8'))
+            message = input("")
+            client.send(message.encode('utf-8'))    
+        elif task ==2:
+            client.send("2".encode('utf-8'))
+            file_name = input("")
+            dir = input("")
+            client.send(file_name.ljust(1024).encode('utf-8'))
+            client.send((str(os.path.getsize(dir))).ljust(1024).encode('utf-8'))
+            with open(dir,'rb') as image_file:
+                while True:
+                    data = image_file.read(1024)
+                    if not data:
+                        break
+                    client.send(data)
+            print('The file has been sent')
+
+handler()
+
+#creating thread for receiving messages
+thread_receive = threading.Thread(target=receive)
+thread_receive.start()
+
+send()
+
+print("Client is closed")
+client.close()
---
+++ server_tcp.py
@@ -0,0 +1,104 @@
+from socket import *
+import threading
+import json
+import os
+
+mutex = threading.Lock()
+
+serverport = 12004
+server = socket(AF_INET , SOCK_STREAM)
+server.bind(('',serverport))
+
+client_Name = {}
+
+def broadcast(Name,data):
+    for i in client_Name:
+        if i != Name:
+            client_Name[i][0].send(data.ljust(1024).encode('utf-8'))
+
+
+def close_connection(client,addr,Name):
+    client.close()
+    client_Name.pop(Name)
+    encoded_message = json.dumps([1,0,Name])
+    broadcast(Name,encoded_message)
+
+def communication_handel(client,addr,Name):
+    global stop_thread
+    while True:
+        task = client.recv(1024).decode('utf-8')
+        if not task:
+            close_connection(client,addr,Name)
+            break
+
+        if task == '1':
+            sentence = client.recv(1024).decode('utf-8')
+            encoded_message = json.dumps([1,2,Name,sentence])
+            broadcast(Name,encoded_message)
+
+        elif task == '2':
+            # mutex.acquire()
+            file_name = client.recv(1024).decode('utf-8').strip()
+            
+            if not file_name:
+                close_connection(client,addr,Name)
+                break
+            file_len = int(client.recv(1024).decode('utf-8').strip())
+
+            if not file_len:
+                close_connection(client,addr,Name)
+                break
+            encoded_message = json.dumps([2,Name,file_name])
+            
+            broadcast(Name,encoded_message)
+            broadcast(Name,str(file_len))
+            
+            while file_len > 0:
+                data = client.recv(1024)
+                file_len-= len(data)
+
+                if not data:
+                    close_connection(client,addr,Name)
+                    break
+                
+                for i in client_Name:
+                    if i!= Name:
+                        client_Name[i][0].send(data)
+            
+            # mutex.release()
+
+def connection_handel(client,addr):
+    limit = 0
+    while True:
+        client.send("Name".ljust(1024).encode('utf-8'))
+        Name = client.recv(1024).decode('utf-8')
+        Name = Name.title()
+        if Name in client_Name:
+            if limit == 3:
+                client.send("Terminate".ljust(1024).encode('utf-8'))
+                client.close()
+                exit()
+            client.send("Retry".ljust(1024).encode('utf-8'))
+            limit += 1        
+        else:
+            client_Name[Name] = [client,addr]
+            client.send("Success".encode('utf-8'))
+            encoded_message = json.dumps([1,1,Name])
+            broadcast(Name,encoded_message)
+            break
+    communication_handel(client,addr,Name)
+
+
+def connect():
+    server.listen()
+    while True:
+        client,addr = server.accept()
+        thread_receive = threading.Thread(target=connection_handel,args=(client,addr))
+        thread_receive.start()
+
+print("The server is ready to receive")
+
+connect()   
+
+print("Server is closed")
+server.close()
